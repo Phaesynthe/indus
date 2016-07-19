47c1d3d3093a98010de584e473b4d28798f31366
Matthew Jackson <mwj8410@gmail.com>
2016-07-19 02:13:21 +0000

    feat(map): add map node create and add

--- app/map/map.js
+++ app/map/map.js
@@ -1,3 +1,26 @@
-module.exports = {
-  test: 'value'
+'use strict';
+
+const _ = require('lodash');
+
+var MapNode = class {
+  constructor (nodeData) {
+    this.id = nodeData.id;
+  }
 };
+
+module.exports = (function () {
+  // The map is constructed of nodes that are connected by junctions
+  // This will initially be used to explor the structure of these entities
+
+  var nodes = [];
+
+  return {
+    loadNode (nodeData) {
+      nodes.push( new MapNode (nodeData));
+    },
+    getNode (nodeId) {
+      return _(nodes).filter({ id: nodeId }).value()[0];
+    }
+  };
+
+})();
--- app/map/map.spec.js
+++ app/map/map.spec.js
@@ -1,10 +1,29 @@
 const expect = require('expect');
+const mapNodeFixture = require('../../testHelpers/fixtures/mapNode');
 const map = require('./map');
 
 describe('map', () => {
 
   it('exists', () => {
-    expect(map.test).toBe('value');
+    expect(typeof map).toBe('object');
+  });
+
+  it('exposes a method for loading map nodes', () => {
+    expect(typeof map.loadNode).toBe('function');
+  });
+
+  it('exposes a method for retrieving map nodes', () => {
+    expect(typeof map.getNode).toBe('function');
+  });
+
+  it('imports a map node correctly', () => {
+    map.loadNode(mapNodeFixture);
+    expect(map.getNode(mapNodeFixture.id).id).toBe(mapNodeFixture.id);
+  });
+
+  it('imports a map node correctly when no data is provided', () => {
+    map.loadNode(mapNodeFixture);
+    expect(map.getNode()).toBe(undefined);
   });
 
 });
